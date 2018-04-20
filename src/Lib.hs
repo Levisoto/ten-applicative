9ea79445bcbbf44aa5730e865d3e2b1e13042c4a
Levisoto <levisoto.337@gmail.com>
2018-04-19 21:11:00 -0500

    added a Paser file

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import AParser
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+-- Problem 1
+-- I need to implement a Functor instance of the Parser
+instance Functor (Parser a) where
+  fmap f (Parser a) = 
+  
