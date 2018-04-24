4329dc9146534d3a2033fbb2db6ee26785f5c3b1
Levisoto <levisoto.337@gmail.com>
2018-04-24 00:34:38 -0500

    I completed 1st week

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,6 +8,18 @@ someFunc = putStrLn "someFunc"
 
 -- Problem 1
 -- I need to implement a Functor instance of the Parser
-instance Functor (Parser a) where
-  fmap f (Parser a) = 
-  
+--hint: function First
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (a,c) = (f a,c)
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser (fmap (first f).p)
+
+  -- fmap f (Parser p) = Parser (\input -> case (p input) of
+  --                                               Just (x,xs) -> Just (f x, xs)
+  --                                               Nothing -> Nothing
+  --                            )
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+  --
+  --
